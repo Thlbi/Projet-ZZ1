6b0e8da2cd3d55947172c0c286af0869b3d54525
Thibaud LEROY <thibaud.leroy@etu.uca.fr>
2021-06-14 15:12:18 +0200

    triangle clignotant

--- exercice6.c
+++ exercice6.c
@@ -36,45 +36,46 @@ void end_sdl(char ok,                                                 // fin nor
 void draw(SDL_Renderer *renderer, int width,int height, int taille)
 {
     SDL_Rect rectangle; 
+
+    
     SDL_SetRenderDrawColor(renderer,                                
-                              0, 0, 120,                               // mode Red, Green, Blue (tous dans 0..255)
+                              120, 0, 0,                               // mode Red, Green, Blue (tous dans 0..255)
                               255);                                   // 0 = transparent ; 255 = opaque
-    rectangle.x = 0;                                                    // x haut gauche du rectangle
-    rectangle.y = 0;                                                    // y haut gauche du rectangle
-    rectangle.w = width;                                                  // sa largeur (w = width)
-    rectangle.h = height;                                                  // sa hauteur (h = height)
+    
+    rectangle.x = 200;                                                    // x haut gauche du rectangle
+    rectangle.y = 200;                                                    // y haut gauche du rectangle
+    rectangle.w = 600;                                                  // sa largeur (w = width)
+    rectangle.h = 600;                                                  // sa hauteur (h = height)
 
-    SDL_RenderFillRect(renderer, &rectangle);
+    SDL_RenderDrawRect(renderer, &rectangle);
 
-    SDL_SetRenderDrawColor(renderer, 26, 168, 43, 255);
-    SDL_RenderDrawLine(renderer,
-                          width/2, 0,                                       // x,y du point de la première extrémité
-                          width/2, height);                                  // x,y seconde extrémité
-    SDL_RenderDrawLine(renderer,
-                          0, height/2,                                       // x,y du point de la première extrémité
-                          width, height/2);
+
+    SDL_SetRenderDrawColor(renderer,0,0,0,255);
 }
 
 void draw2(SDL_Renderer *renderer, int width,int height, int taille)
 {
     SDL_Rect rectangle; 
+
+    
     SDL_SetRenderDrawColor(renderer,                                
                               0, 0, 120,                               // mode Red, Green, Blue (tous dans 0..255)
-                              255);                                   // 0 = transparent ; 255 = opaque
+                              255);      
+                                                          // 0 = transparent ; 255 = opaque
     rectangle.x = 0;                                                    // x haut gauche du rectangle
     rectangle.y = 0;                                                    // y haut gauche du rectangle
-    rectangle.w = width;                                                  // sa largeur (w = width)
-    rectangle.h = width;                                                  // sa hauteur (h = height)
+    rectangle.w = 400;                                                  // sa largeur (w = width)
+    rectangle.h = 400;                                                  // sa hauteur (h = height)
 
-    SDL_RenderFillRect(renderer, &rectangle);
+    SDL_RenderDrawRect(renderer, &rectangle);
 
-    SDL_SetRenderDrawColor(renderer, 26, 168, 43, 255);
+    /*
     SDL_RenderDrawLine(renderer,
                           0, 0,                                       // x,y du point de la première extrémité
                           width, height);                                  // x,y seconde extrémité
-    SDL_RenderDrawLine(renderer,
-                          width, 0,                                       // x,y du point de la première extrémité
-                          0, height);
+    */
+
+    SDL_SetRenderDrawColor(renderer,0,0,0,255);
 }
 
 
@@ -102,15 +103,13 @@ int main(int argc, char **argv) {
 
 
   SDL_GetCurrentDisplayMode(0, &screen);
-  printf("Résolution écran\n\tw : %d\n\th : %d\n", screen.w,
-              screen.h);
 
   int width=screen.w,height=screen.h,taille=10;
 
   window = SDL_CreateWindow("Premier dessin",
                             SDL_WINDOWPOS_CENTERED,
-                            SDL_WINDOWPOS_CENTERED, screen.w ,
-                            screen.h ,
+                            SDL_WINDOWPOS_CENTERED, screen.w*0.66 ,
+                            screen.h*0.66 ,
                             SDL_WINDOW_OPENGL);
   if (window == NULL) end_sdl(0, "ERROR WINDOW CREATION", window, renderer);
 
@@ -121,7 +120,8 @@ int main(int argc, char **argv) {
 
 
 /*********************************************************************************************************************/
-  /*                                     On dessine dans le renderer                                                   */
+  /*                            On dessine dans le renderer                                                   */
+  
   draw(renderer,width,height,taille);                                                     // appel de la fonction qui crée l'image  
   SDL_RenderPresent(renderer);                                        // affichage                                                  // Pause exprimée en ms
   while (continuer)
@@ -137,9 +137,11 @@ int main(int argc, char **argv) {
       draw2(renderer,width,height,taille);
       SDL_RenderPresent(renderer);
       SDL_Delay(100);
+      SDL_RenderClear(renderer);
       draw(renderer,width,height,taille);
       SDL_RenderPresent(renderer);
       SDL_Delay(100);
+      SDL_RenderClear(renderer);
 
   }
 
