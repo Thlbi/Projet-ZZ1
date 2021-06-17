2c91890f598555d6e033f6476e1de24355192095
Valentin MEUNIER <valentin.meunier@etu.uca.fr>
2021-06-17 17:21:00 +0200

    dossier

---
+++ SDL_premier_exo/version.c
@@ -0,0 +1,12 @@
+#include <SDL2/SDL.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+  (void)argc;
+  (void)argv;
+  SDL_version nb;
+  SDL_VERSION(&nb);
+
+  printf("Version de la SDL : %d.%d.%d\n", nb.major, nb.minor, nb.patch);
+  return 0;
+}
