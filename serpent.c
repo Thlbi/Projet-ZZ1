bae797316ee718bfd236d56d4d49f9455bb969d9
Marian POIROT <marian.poirot@etu.uca.fr>
2021-06-17 09:52:16 +0200

    serpent nettement amélioré

--- serpent.c
+++ serpent.c
@@ -5,6 +5,10 @@
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
+#include "serpent.h"
+
+#define NB_SERPENT 1
+#define NB_CASE 10
 
 void end_sdl(char ok,char const* msg,SDL_Window* window, SDL_Renderer* renderer) 
 {
@@ -29,31 +33,83 @@ void end_sdl(char ok,char const* msg,SDL_Window* window, SDL_Renderer* renderer)
   }                                                               
 }  
 
-void dessin(SDL_Renderer* renderer,int frame)
+void dessin(SDL_Renderer* renderer,cellule_t ** tab,int numero_serpent,int aleat)
 {
-	int x=50,y=50,x1,y1,z;
+	int x=50,y=50,x1,y1,z=0,i=0;
+	cellule_t ** prec=tab;
 
-	for (int i=0; i<17;i++)
+	while (*prec!=NULL)
 	{
-		if (17-frame>=i)
-		{
-			z=rand()%30;
-			x1=(x+z)%1500;      
-			y1=(y+30-z)%800;
-		}
+		if (aleat) z=rand()%13;
+		if (z>9) z=-z;
+		printf("%d %d \n",(*prec)->x,(*prec)->y);
+		x1=x+(*prec)->x+z;
+		y1=y+(*prec)->y-z;
+		(*prec)->x+=z;
+		(*prec)->y-=z;
+		prec=&(*prec)->suiv;
 
   		SDL_SetRenderDrawColor(renderer,30*i, 50, i,255);   
   		SDL_RenderDrawLine(renderer, x,y,x1,y1);
+
 		SDL_RenderPresent(renderer);
 		x=x1;
 		y=y1;
-		SDL_Delay(10);
+		i=i+1;
+		SDL_Delay(1);
 	}
   	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
 }
 
+cellule_t * allouer(int num,int coord_x,int coord_y)
+{
+        cellule_t * nouv=(cellule_t *)malloc(sizeof(cellule_t));
+        if(nouv!=NULL)
+   {
+      nouv->num_serpent=num;
+      nouv->x=coord_x;
+      nouv->y=coord_y;
+        }
+        return nouv;
+}
+
+void adj_cell(cellule_t **prec,cellule_t *nouv)
+{
+                *prec=nouv;
+}
+
+void SuppCell(cellule_t **prec)
+{
+        cellule_t * cour=*prec;
+        *prec=(*prec)->suiv;
+        free(cour);
+}
+
+void init_serpent(cellule_t ** tab,int numero_serpent)
+{
+        int x=50+50*numero_serpent,y=50+50*numero_serpent,x1,y1,z;
+	cellule_t ** prec=tab;
+	cellule_t * nouv;
+
+        for (int i=0; i<17;i++)
+        {
+                if (17-NB_CASE>=i)
+                {
+                        z=rand()%30;
+                        x1=(x+z)%1500;
+                        y1=(y+30-z)%800;
+			nouv=allouer(i,x1,y1);
+			adj_cell(prec,nouv);
+			prec=&nouv->suiv;
+                }
+        }
+}
+
 int main ()
 {
+	cellule_t * tab;
+	int i;
+	
 	srand(time(0));
 	if (SDL_Init(SDL_INIT_VIDEO) == -1)
 	{
@@ -80,17 +136,23 @@ int main ()
     	{
         	fprintf(stderr, "Erreur d'initialisation de la SDL : %s\n", SDL_GetError());
     	}
+	for (i=0;i<NB_SERPENT;i++)
+	{
+		init_serpent(&tab,i);
+	}
 	SDL_Event event;
+	dessin(renderer,&tab,i,0);
+	SDL_Delay(100);
+
 	int running=1;
    	while (running)
     	{
-		for (int i=0;i<18;i++)
+		SDL_RenderClear(renderer);
+		for (i=0;i<NB_SERPENT;i++)
 		{
-
-			dessin(renderer,i);
-			SDL_Delay(100);
-  			SDL_RenderClear(renderer);
+			dessin(renderer,&tab,i,1);
 		}
+
         	while (SDL_PollEvent(&event))
         	{
             		switch (event.type)
@@ -114,7 +176,12 @@ int main ()
 					running = 0;
             		}
         	}
-		SDL_Delay(1);
+		SDL_Delay(100);
     	}
 	end_sdl(1, "Normal ending", window, renderer);
+	for (int i=0;i<NB_SERPENT;i++){
+		while (tab!=NULL){
+			SuppCell(&tab);
+		}
+	}
 }
