d3b7d4d50d73d90611ef68372bd9386aff63f8ea
Marian POIROT <marian.poirot@etu.uca.fr>
2021-06-19 21:40:49 +0200

    graph.dot

--- partition/partition_a.h
+++ partition/partition_a.h
@@ -19,15 +19,22 @@ typedef struct couple{
 partition_t * creer(int taille);
 int recuperer_classe(partition_t * t,int nombre,int taille);
 void fusion(partition_t * t,int x,int y,int taille);
+void fusion_mat(int ** mat,partition_t * t,int taille);
+void fusion_couple(couple_t * c,partition_t *t,int taille);
 int lister_classe(partition_t * t,int classe,int taille,int *tab);
 void lister_partition(partition_t * t,int taille);
 int ** creer_mat(int nb_noeuds);
 int graph_mat(int ** mat,int taille);
+int graph_couple(couple_t *c);
 void generation_mat(int **mat,int taille);	
 void liberer(int **mat,int taille);
 couple_t * init_couple();
 void generer_couple(couple_t *c,int taille);
 int graph_couple(couple_t *c);
+void graph_kruskal(aretes_t *A);
+aretes_t * allouer(int coord1,int coord2,int poids);
+couple_t * ordonner_aretes(couple_t *c);
+void kruskal(int taille);
 int main();
 
 #endif
