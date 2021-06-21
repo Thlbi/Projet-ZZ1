c659adca6b1b55f9622cf52359badcf27f68ed1b
Valentin MEUNIER <valentin.meunier@etu.uca.fr>
2021-06-21 12:14:00 +0200

    keuskam ok

--- kruskal.h
+++ kruskal.h
@@ -3,6 +3,13 @@
 
 #include "tas.h"
 #include "graph_aretes_tab.h"
+
+graph_t * kruskal(graph_t * graph,int noeuds, int nb_aretes,int * cours);
+
+graph_t * Fisher(graph_t * graph, int nb_aretes);
+
+int  generation(graph_t * graph, int n, int p);
+
 int main();
 
 #endif
