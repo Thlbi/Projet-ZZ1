9d8a76f464f46fecc5d6370457b75a2226869b96
Thibaud LEROY <thibaud.leroy@etu.uca.fr>
2021-06-21 18:47:11 +0200

    dans le toute je commit

--- graph/main.c
+++ graph/main.c
@@ -3,14 +3,13 @@
 int main(){
     srand(1998);
     graph_t *graph=initgraph(TAILLE);
-    printf("ok\n");
     generer_couple_2(graph);
     arete_t*A=graph->suiv;
     int **tab;
     graph=ordonner_Fisher(graph);
     
     A=kruskal(graph);
-	//afficher_arete(A);
+	afficher_arete(A);
 
     tab=tableau_aretes(A);
 
